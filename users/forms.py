24e484375206368835b2e0d916838d00f3698dbb
Bartosz Kwiecień <b.kwiecien@gmail.com>
2015-09-26 22:57:14 +0200

    Require password confirmation when registering new user

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,12 @@
 from django import forms
 
 from users.models import AppUser
+# from django.utils.translation import ugettext, ugettext_lazy as _
 
 
 class RegisterUserForm(forms.ModelForm):
+    password2 = forms.CharField(label='Password confirmation',
+                                widget=forms.PasswordInput)
 
     class Meta:
         model = AppUser
@@ -12,8 +15,15 @@ class RegisterUserForm(forms.ModelForm):
 
     def save(self, commit=True):
         user = super(RegisterUserForm, self).save(commit=False)
-        user.set_password(self.cleaned_data["password"])
+        user.set_password(self.cleaned_data['password'])
         if commit:
             user.save()
         return user
 
+    def clean_password2(self):
+        password = self.cleaned_data.get('password')
+        password2 = self.cleaned_data.get('password2')
+        if password and password2 and password != password2:
+            raise forms.ValidationError('Passwords do not match',
+                                        code='invalid')
+        return password2
